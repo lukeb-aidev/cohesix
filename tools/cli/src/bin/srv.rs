ab3f1fe07fe5e79c97b8bfd756c4b69bb9e5dadc
lukeb-aidev <lukas.bower@gmail.com>
2025-06-21 20:15:42 +1000

    Add Plan9 CLI tools and Secure9P handshake stubs

---
+++ tools/cli/src/bin/srv.rs
@@ -0,0 +1,30 @@
+// CLASSIFICATION: COMMUNITY
+// Filename: srv.rs v0.1
+// Author: Lukas Bower
+// Date Modified: 2026-01-27
+
+use clap::Parser;
+use cohesix::trace::recorder::event;
+
+/// Register a service address under /srv
+#[derive(Parser)]
+#[command(about = "Create /srv entry for a service")]
+struct Args {
+    /// Name of the service
+    name: String,
+    /// Address in format tcp:HOST:PORT or unix:/path
+    address: String,
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+    std::fs::create_dir_all("/srv")?;
+    std::fs::write(format!("/srv/{}", args.name), &args.address)?;
+    event(
+        "srv",
+        "register",
+        &format!("{} {}", args.name, args.address),
+    );
+    println!("registered {} => {}", args.name, args.address);
+    Ok(())
+}
