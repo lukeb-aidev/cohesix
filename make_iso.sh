b1d0c45d421ecfc9808a5dd677c7fda31d70f317
lukeb-aidev <lukas.bower@gmail.com>
2025-06-21 11:59:53 +1000

    Update ISO wrapper

--- make_iso.sh
+++ make_iso.sh
@@ -1,9 +1,9 @@
 // CLASSIFICATION: COMMUNITY
-# Filename: make_iso.sh v0.8
+# Filename: make_iso.sh v0.9
 # Author: Lukas Bower
-# Date Modified: 2025-12-17
+# Date Modified: 2026-01-06
 #!/bin/bash
 set -eu
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-exec bash "$SCRIPT_DIR/scripts/make_iso.sh" "$@"
+exec bash "$SCRIPT_DIR/scripts/make_grub_iso.sh" "$@"
