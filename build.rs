fd5044afb4cf21174366717c8228361acbcc74eb
lukeb-aidev <lukas.bower@gmail.com>
2025-06-23 19:20:33 +1000

    fix: build cuda and efi issues

--- build.rs
+++ build.rs
@@ -1,12 +1,26 @@
 // CLASSIFICATION: COMMUNITY
-// Filename: build.rs v0.3
+// Filename: build.rs v0.4
 // Author: Lukas Bower
-// Date Modified: 2026-02-16
+// Date Modified: 2026-07-22
 
 fn main() {
-    use std::process::Command;
+    use std::{env, process::Command};
 
     println!("cargo:rerun-if-changed=tests/gpu_demos/add.cu");
+    env::set_var("CUDA_HOME", "/usr");
+    if let Ok(p) = env::var("PATH") {
+        env::set_var("PATH", format!("/usr/bin:{}", p));
+    } else {
+        env::set_var("PATH", "/usr/bin");
+    }
+    if let Ok(ld) = env::var("LD_LIBRARY_PATH") {
+        env::set_var(
+            "LD_LIBRARY_PATH",
+            format!("/usr/lib/aarch64-linux-gnu:{}", ld),
+        );
+    } else {
+        env::set_var("LD_LIBRARY_PATH", "/usr/lib/aarch64-linux-gnu");
+    }
     let coh_gpu = std::env::var("COH_GPU").unwrap_or_default();
 
     if coh_gpu == "1" {
