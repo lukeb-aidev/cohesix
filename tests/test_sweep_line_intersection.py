8d49e7fcea53e3b21fc65dd9cc5a240339f9559f
Lukas Bower <lukas.bower@gmail.com>
2025-06-30 12:54:33 +1000

    tests: use floats in sweep line data

--- tests/test_sweep_line_intersection.py
+++ tests/test_sweep_line_intersection.py
@@ -1,7 +1,7 @@
 # CLASSIFICATION: COMMUNITY
-# Filename: test_sweep_line_intersection.py v0.1
+# Filename: test_sweep_line_intersection.py v0.2
 # Author: Lukas Bower
-# Date Modified: 2026-11-19
+# Date Modified: 2026-11-20
 """Unit tests for sweep-line self-intersection detection."""
 
 import unittest
@@ -12,9 +12,9 @@ from src.geometry.sweep_line_intersection import find_self_intersections
 class SweepLineIntersectionTest(unittest.TestCase):
     def test_basic_intersection(self) -> None:
         segments = [
-            ((0, 0), (5, 5)),
-            ((0, 5), (5, 0)),
-            ((1, 1), (2, 2)),
+            ((0.0, 0.0), (5.0, 5.0)),
+            ((0.0, 5.0), (5.0, 0.0)),
+            ((1.0, 1.0), (2.0, 2.0)),
         ]
         pts = find_self_intersections(segments)
         self.assertEqual(len(pts), 1)
@@ -22,7 +22,7 @@ class SweepLineIntersectionTest(unittest.TestCase):
         self.assertAlmostEqual(pts[0][1], 2.5, places=6)
 
     def test_no_intersections(self) -> None:
-        segments = [((0, 0), (1, 0)), ((2, 0), (3, 0))]
+        segments = [((0.0, 0.0), (1.0, 0.0)), ((2.0, 0.0), (3.0, 0.0))]
         pts = find_self_intersections(segments)
         self.assertEqual(pts, [])
 
