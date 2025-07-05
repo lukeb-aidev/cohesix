04dd6bcb7fbb9358e798e8fc6f3a40e20122eb28
Lukas Bower <lukas.bower@gmail.com>
2025-07-05 11:59:00 +1000

    Add generic Option C library

--- test/test_c_main.c
+++ test/test_c_main.c
@@ -1,13 +1,13 @@
 // CLASSIFICATION: COMMUNITY
 // Filename: test_c_main.c v0.1
 // Author: Lukas Bower
-// Date Modified: 2025-07-22
+// Date Modified: 2026-12-31
 // SPDX-License-Identifier: MIT
 
 #include "../src/c/placeholder.c"
 #include <assert.h>
 
 int main(void) {
-    assert(placeholder() == 0);
+    assert(placeholder() == 1);
     return 0;
 }
