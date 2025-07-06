5a211cf1b6636fb7acf004e2823004c9b4a7b84a
Lukas Bower <lukas.bower@gmail.com>
2025-07-06 20:18:49 +1000

    Convert cohesix_root to no_std

--- src/bin/cohesix_root.rs
+++ src/bin/cohesix_root.rs
@@ -1,15 +1,40 @@
 // CLASSIFICATION: COMMUNITY
-// Filename: cohesix_root.rs v0.3
+// Filename: cohesix_root.rs v0.4
 // Author: Lukas Bower
-// Date Modified: 2027-08-08
+// Date Modified: 2027-08-13
 #![no_std]
 #![no_main]
+
+extern crate alloc;
+
 use core::panic::PanicInfo;
+use alloc::{string::String, vec::Vec};
+use libm::sqrtf;
+
+use cohesix::{println, sh_loop::run};
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    loop {}
+    main();
+    loop {
+        core::hint::spin_loop();
+    }
 }
+
+fn main() {
+    println!("COHESIX_BOOT_OK");
+    println!("[root] booting...");
+    let roots: Vec<String> = Vec::new();
+    println!("[root] {} mounts", roots.len());
+    println!("[root] sqrt(2) = {}", sqrtf(2.0));
+    println!("[root] launching shell");
+    run();
+}
+
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+    coherr!("[root] panic");
+    loop {
+        core::hint::spin_loop();
+    }
 }
