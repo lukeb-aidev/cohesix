ba966511fc17548af2df7d3bf9426d5bff62d18d
lukeb-aidev <lukas.bower@gmail.com>
2025-07-13 15:48:52 +1000

    sel4 build fixes

--- third_party/seL4/build_config.cmake
+++ third_party/seL4/build_config.cmake
@@ -1,10 +1,16 @@
 # CLASSIFICATION: COMMUNITY
-# Filename: build_config.cmake v0.1
+# Filename: build_config.cmake v0.2
 # Author: Lukas Bower
 # Date Modified: 2027-12-31
 
-set(PLATFORM "qemu_arm_virt" CACHE STRING "")
-set(KernelArch "aarch64" CACHE STRING "")
-set(KernelWordSize 64 CACHE STRING "")
+set(PLATFORM "qemu-arm-virt" CACHE STRING "")
+set(KernelArch "ARM" CACHE STRING "")
+set(KernelArchArmV8a ON CACHE BOOL "")
 set(KernelSel4Arch "aarch64" CACHE STRING "")
+set(KernelSel4ArchAarch64 ON CACHE BOOL "")
+set(KernelSel4ArchArmHyp OFF CACHE BOOL "")
+set(KernelWordSize 64 CACHE STRING "")
+set(AARCH64 ON CACHE BOOL "")
+set(SIMULATION ON CACHE BOOL "")
+set(RELEASE ON CACHE BOOL "")
 set(CROSS_COMPILER_PREFIX "aarch64-linux-gnu-" CACHE STRING "")
