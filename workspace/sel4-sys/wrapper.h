4f93f4a054437f403601e70df9cf45d20438db8d
Lukas Bower <lukas.bower@gmail.com>
2025-07-20 07:12:27 +1000

    Update sel4-sys bindings for 64-bit headers

--- workspace/sel4-sys/wrapper.h
+++ workspace/sel4-sys/wrapper.h
@@ -1,11 +1,9 @@
 // CLASSIFICATION: COMMUNITY
-// Filename: wrapper.h v0.1
+// Filename: wrapper.h v0.2
 // Author: Lukas Bower
-// Date Modified: 2027-12-31
+// Date Modified: 2028-08-31
 
-typedef unsigned long long seL4_Word;
-typedef unsigned char uint8_t;
+#include "sel4/config.h"
+#include <sel4/bootinfo_types.h>
+#include <sel4/syscall.h>
 
-static inline void seL4_DebugPutChar(uint8_t c) { (void)c; }
-static inline void seL4_Yield(void) {}
-static inline void seL4_DebugHalt(void) {}
