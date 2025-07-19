8fe192b5fdb641c2b62a4c24a6bbc4b40e49b73f
Lukas Bower <lukas.bower@gmail.com>
2025-07-19 15:02:04 +1000

    cohesix_root: generate sel4 bindings

---
+++ workspace/cohesix_root/wrapper.h
@@ -0,0 +1,8 @@
+// CLASSIFICATION: COMMUNITY
+// Filename: wrapper.h v0.1
+// Author: Lukas Bower
+// Date Modified: 2028-02-15
+
+#include <sel4/types.h>
+#include <sel4/bootinfo_types.h>
+#include <sel4/constants.h>
