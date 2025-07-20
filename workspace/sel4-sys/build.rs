f1d3abbb497459360d6a14742c7cf2b77805cd1c
Lukas Bower <lukas.bower@gmail.com>
2025-07-20 21:23:27 +1000

    sel4 header path fixes

--- workspace/sel4-sys/build.rs
+++ workspace/sel4-sys/build.rs
@@ -4,15 +4,22 @@
 // Date Modified: 2028-09-10
 
 use std::{env, path::PathBuf};
+#[path = "../sel4_paths.rs"]
+mod sel4_paths;
 
 fn main() {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     let cflags = env::var("SEL4_SYS_CFLAGS").unwrap_or_default();
-    let clang_args: Vec<&str> = cflags.split_whitespace().collect();
+    let mut clang_args: Vec<String> = cflags.split_whitespace().map(|s| s.to_string()).collect();
+
+    if let Ok(gen_hdr) = env::var("SEL4_GEN_HDR") {
+        clang_args.push(format!("-I{}", gen_hdr));
+        clang_args.push(format!("-I{}/sel4", gen_hdr));
+    }
 
     let bindings = bindgen::Builder::default()
         .header("include/wrapper.h")
-        .clang_args(clang_args)
+        .clang_args(&clang_args)
         .use_core()
         .ctypes_prefix("cty")
         .generate()
