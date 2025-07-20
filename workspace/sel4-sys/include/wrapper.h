e40a34d38d234e5e25fc2296238f2115eee6ce6c
Lukas Bower <lukas.bower@gmail.com>
2025-07-20 18:03:22 +1000

    Update build setup for sel4

--- workspace/sel4-sys/include/wrapper.h
+++ workspace/sel4-sys/include/wrapper.h
@@ -1,9 +1,8 @@
 // CLASSIFICATION: COMMUNITY
-// Filename: wrapper.h v1.40
+// Filename: wrapper.h v1.41
 // Author: Lukas Bower
-// Date Modified: 2028-09-08
+// Date Modified: 2028-09-10
 
 #include "stub_types.h"
 #include <sel4/sel4.h>
-#include <sel4/syscalls.h>
-
+#include <sel4_arch/sel4_arch.h>
