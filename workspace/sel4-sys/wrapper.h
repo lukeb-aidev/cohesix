0f1904c8eb87d1b62e34f6c104843f3203fd816b
Lukas Bower <lukas.bower@gmail.com>
2025-07-20 15:25:09 +1000

    FixSel4SysBuild-104 update build flags

--- workspace/sel4-sys/wrapper.h
+++ workspace/sel4-sys/wrapper.h
@@ -1,9 +1,9 @@
 // CLASSIFICATION: COMMUNITY
-// Filename: wrapper.h v1.39
+// Filename: wrapper.h v1.40
 // Author: Lukas Bower
-// Date Modified: 2028-09-07
+// Date Modified: 2028-09-08
 
 #include <sel4/sel4.h>
 #include <sel4/syscalls.h>
-#include <sel4_arch/constants.h>
+#include <sel4/sel4_arch/constants.h>
 
