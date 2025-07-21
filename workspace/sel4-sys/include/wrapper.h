9fa085e84e5adeb57f0c31f8b530cf830e4fda03
Lukas Bower <lukas.bower@gmail.com>
2025-07-21 13:11:11 +1000

    chore: unify seL4 include roots

--- workspace/sel4-sys/include/wrapper.h
+++ workspace/sel4-sys/include/wrapper.h
@@ -1,17 +1,15 @@
 // CLASSIFICATION: COMMUNITY
 // Filename: wrapper.h v1.45
 // Author: Lukas Bower
-// Date Modified: 2025-07-21
+// Date Modified: 2028-11-08
 
+#pragma once
+
+// Bring in generated CONFIG_ macros:
 #include <autoconf.h>
-#include <sel4/sel4/config.h>
 #ifndef seL4_WordSizeBits
 #define seL4_WordSizeBits CONFIG_WORD_SIZE
 #endif
+
+// Core seL4 API:
 #include <sel4/sel4.h>
-#include <sel4/config.h>
-#include <sel4/gen_config.h>
-#include <sel4/constants.h>
-#include <sel4/syscall.h>
-#include <sel4/sel4_arch/constants.h>
-#include "stub_types.h"
