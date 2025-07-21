9b1a7fde19317e8afc82d907d23b6109cf5a9d1e
Lukas Bower <lukas.bower@gmail.com>
2025-07-22 09:22:58 +1000

    Implement boot plumbing audit actions

---
+++ workspace/sel4-sys-extern-wrapper/include/sel4_wrapper.h
@@ -0,0 +1,11 @@
+// CLASSIFICATION: COMMUNITY
+// Filename: sel4_wrapper.h v0.1
+// Author: Lukas Bower
+// Date Modified: 2028-11-21
+
+#pragma once
+
+#include <sel4/config.h>
+#include <sel4/sel4.h>
+#include <sel4/bootinfo.h>
+#include <sel4/bootinfo_types.h>
