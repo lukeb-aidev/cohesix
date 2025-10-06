cebe2c533691ec27a3be42c4288d39acf1b065ec
lukeb-aidev <lukas.bower@gmail.com>
2025-10-06 14:57:47 +1100

    Boot fixes

--- workspace/cohesix_root/src/startup.rs
+++ workspace/cohesix_root/src/startup.rs
@@ -1,9 +1,10 @@
 // CLASSIFICATION: COMMUNITY
 // Filename: startup.rs v0.3
 // Author: Lukas Bower
-// Date Modified: 2028-01-22
+// Date Modified: 2025-10-06
 
 use core::arch::global_asm;
+use sel4_sys_extern_wrapper::seL4_DebugPutChar;
 
 // Exception vector base is provided by `vec.S` and linked into `.vectors`.
 // The kernel maps this region at the address configured in `link.ld`.
@@ -21,6 +22,9 @@ rust_entry:
 /// Rust entry point invoked from `entry.S`.
 #[no_mangle]
 pub extern "C" fn rust_start() -> ! {
+    unsafe {
+        seL4_DebugPutChar(b'C' as i32);
+    }
     extern "C" {
         fn main();
     }
