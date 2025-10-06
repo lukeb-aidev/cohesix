6d9e9070fcb5aa0e07d60d8e6c56e9e0213ff429
lukeb-aidev <lukas.bower@gmail.com>
2025-10-06 13:19:49 +1100

    Build fixes

--- workspace/cohesix_root/src/mmu.rs
+++ workspace/cohesix_root/src/mmu.rs
@@ -1,55 +1,8 @@
 // CLASSIFICATION: COMMUNITY
 // Filename: mmu.rs v0.2
 // Author: Lukas Bower
-// Date Modified: 2028-02-15
+// Date Modified: 2025-10-06
 #![allow(static_mut_refs)]
-
-#[repr(align(4096))]
-struct Table([u64; 512]);
-
-static mut L1_TABLE: Table = Table([0; 512]);
-static mut L2_TABLE: Table = Table([0; 512]);
-
-const BLOCK_FLAGS: u64 = 0b11; // AF=1 | SH=0 | AP=00 | AttrIdx=0
-const DEVICE_FLAGS: u64 = 0b11 | (1 << 2); // device memory attr index 1
-
-fn init_tables(
-    l1: &mut [u64; 512],
-    l2: &mut [u64; 512],
-    dtb: usize,
-    dtb_end: usize,
-    bootinfo: usize,
-    bootinfo_end: usize,
-) {
-    for entry in l1.iter_mut() {
-        *entry = 0;
-    }
-    for entry in l2.iter_mut() {
-        *entry = 0;
-    }
-
-    l1[0] = (l2.as_ptr() as u64) | 0b11;
-
-    for i in 0..16 {
-        l2[i] = ((i as u64) << 21) | BLOCK_FLAGS;
-    }
-
-    let dtb_idx = dtb >> 21;
-    let dtb_end_idx = (dtb_end + 0x1FFFFF) >> 21;
-    for i in dtb_idx..dtb_end_idx {
-        if i < 512 {
-            l2[i] = ((i as u64) << 21) | DEVICE_FLAGS;
-        }
-    }
-
-    let bi_idx = bootinfo >> 21;
-    let bi_end_idx = (bootinfo_end + 0x1FFFFF) >> 21;
-    for i in bi_idx..bi_end_idx {
-        if i < 512 {
-            l2[i] = ((i as u64) << 21) | BLOCK_FLAGS;
-        }
-    }
-}
 
 pub unsafe fn init(
     _text_start: usize,
@@ -59,33 +12,18 @@ pub unsafe fn init(
     bootinfo: usize,
     bootinfo_end: usize,
 ) {
-    init_tables(
-        &mut L1_TABLE.0,
-        &mut L2_TABLE.0,
-        dtb,
-        dtb_end,
-        bootinfo,
-        bootinfo_end,
-    );
-    core::arch::asm!(
-        "msr TTBR0_EL1, {0}",
-        "dsb ishst",
-        "isb",
-        in(reg) (&L1_TABLE as *const _ as u64),
-        options(nostack, preserves_flags)
-    );
+    let _ = (dtb, dtb_end, bootinfo, bootinfo_end);
+    crate::coherr!("mmu:skipping_ttbr_setup dtb={:#x} bootinfo={:#x}", dtb, bootinfo);
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::init;
 
     #[test]
-    fn tables_init_match_snapshot() {
-        let mut l1 = [0u64; 512];
-        let mut l2 = [0u64; 512];
-        init_tables(&mut l1, &mut l2, 0x300000, 0x350000, 0x4_0000, 0x4_1000);
-        assert_eq!(l1[0], (l2.as_ptr() as u64) | 0b11);
-        assert_eq!(l2[0], BLOCK_FLAGS);
+    fn init_is_noop_under_sel4() {
+        unsafe {
+            init(0, 0, 0, 0, 0, 0);
+        }
     }
 }
