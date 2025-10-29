4b90f945fdc81af6c0bcc2ebd1ff66259a4a40eb
Lukas Bower <lukas.bower@gmail.com>
2025-10-29 22:19:51 +1100

    Fix root task CNode depth conversions

--- apps/root-task/src/sel4_view.rs
+++ apps/root-task/src/sel4_view.rs
@@ -2,7 +2,7 @@
 #![allow(dead_code)]
 #![allow(unsafe_code)]
 
-use sel4_sys::{seL4_BootInfo, seL4_CPtr, seL4_Word, seL4_CapInitThreadCNode};
+use sel4_sys::{seL4_BootInfo, seL4_CPtr, seL4_CapInitThreadCNode, seL4_Word};
 
 #[inline(always)]
 pub fn init_cnode_cptr(_bi: &seL4_BootInfo) -> seL4_CPtr {
