32ec78c4030c361a3799036d165ee78655e4ca72
Lukas Bower <lukas.bower@gmail.com>
2025-11-23 19:03:48 +1100

    Align sel4-sys bindings with upstream seL4 headers

---
+++ crates/sel4-sys/upstream/libsel4/sel4_arch_include/riscv64/sel4/sel4_arch/types.h
@@ -0,0 +1,11 @@
+/* Author: Lukas Bower */
+/*
+ * Copyright 2020, Data61, CSIRO (ABN 41 687 119 230)
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <sel4/config.h>
+#include <sel4/simple_types.h>
